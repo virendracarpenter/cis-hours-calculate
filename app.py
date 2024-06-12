4e3b514c85c7071d2843b86c4f6426dea0ae9bc0
Virendra Carpenter <66617140+virendracarpenter@users.noreply.github.com>
2024-06-12 22:47:17 +0530

    Update options in app.py

--- app.py
+++ app.py
@@ -36,6 +36,11 @@ async def run_selenium(email, password):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("start-maximized"); 
+    options.add_argument("disable-infobars");
+    options.add_argument("--disable-extensions"); 
+    options.add_argument("--disable-gpu");
+    options.add_argument("--disable-dev-shm-usage");
     driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 20)
 
