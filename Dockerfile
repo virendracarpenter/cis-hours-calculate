257a691a2b5c6be8ccb406a9d079726e10cf08d7
virendra carpenter <viren.28opensky@gmail.com>
2024-06-15 17:29:03 +0530

    changed to use pyppeteer for more speed

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,33 @@
-FROM selenium/standalone-chrome:latest
+# FROM selenium/standalone-chrome:latest
+FROM python:3.12
 
 # Install Python and other necessary packages
-USER root
-RUN apt update && \
-    apt install -y python3 python3-pip
+# USER root
+# RUN apt update && \
+#     apt install -y python3 python3-pip
+
 
 RUN apt -y update
 RUN pip install --upgrade pip
+
+RUN apt-get update && apt-get install -y \
+    libnss3 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxi6 \
+    libxtst6 \
+    libpangocairo-1.0-0 \
+    libxrandr2 \
+    libasound2 \
+    libpango-1.0-0 \
+    libcups2 \
+    libxss1 \
+    libgtk-3-0 \
+    libgbm-dev \
+    curl \
+    && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
 COPY . /app
@@ -17,6 +38,8 @@ COPY requirements.txt .
 # Install the dependencies specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
+RUN pyppeteer-install
+
 # Expose port 80 for the application
 EXPOSE $PORT
 
@@ -24,4 +47,4 @@ EXPOSE $PORT
 ENV PYTHONUNBUFFERED=1
 
 # Run the FastAPI application using uvicorn
-CMD ["sh", "-c", "uvicorn main:app --host 0.0.0.0 --port $PORT"]
+CMD ["sh", "-c", "uvicorn app:app --host 0.0.0.0 --port $PORT"]
--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import re
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from pyppeteer import launch
+from pyppeteer.errors import TimeoutError
 
 LOGINURL = 'https://erp.cisin.com/login.asp'
 COMPTIMESHEETURL = 'https://erp.cisin.com/timesheetnew.asp'
@@ -15,52 +11,40 @@ COMPTIMESHEETURL = 'https://erp.cisin.com/timesheetnew.asp'
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/", response_class=HTMLResponse)
 async def get_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/submit", response_class=HTMLResponse)
 async def submit_form(request: Request, email: str = Form(...), password: str = Form(...)):
-    result = await run_selenium(email, password)
+    result = await run_pyppeteer(email, password)
     if result: 
         return templates.TemplateResponse("result.html", {"request": request, "result": result})
     return templates.TemplateResponse("result.html", {"request": request, "result": "Invalid Credentials"})
 
-
-async def run_selenium(email, password):
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("start-maximized")
-    options.add_argument("disable-infobars")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(options=options)
-    wait = WebDriverWait(driver, 20)
+async def run_pyppeteer(email, password):
+    browser = await launch(headless=True, args=["--no-sandbox", "--disable-dev-shm-usage", "--disable-extensions", "--disable-gpu"])
+    page = await browser.newPage()
 
     try:
-        driver.get(LOGINURL)
-        driver.find_element(By.NAME, 'uname').send_keys(email)
-        driver.find_element(By.NAME, 'pass').send_keys(password)
-        driver.find_element(By.CSS_SELECTOR, 'input.submit-login').click()
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body')))
+        await page.goto(LOGINURL)
+        await page.type('input[name=uname]', email)
+        await page.type('input[name=pass]', password)
+        await page.click('input.submit-login')
+        await page.waitForSelector('body', timeout=10000)
 
-        driver.get(COMPTIMESHEETURL)
-        wait.until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, 'table#product-table')))
-        tables = driver.find_elements(By.CSS_SELECTOR, 'table#product-table')
+        await page.goto(COMPTIMESHEETURL)
+        await page.waitForSelector('table#product-table', timeout=10000)
+        tables = await page.querySelectorAll('table#product-table')
 
         if tables:
             total_extra_minutes = 0
-            rows = tables[0].find_elements(By.CSS_SELECTOR, 'tr')
+            rows = await tables[0].querySelectorAll('tr')
 
             for row in rows:
-                cells = row.find_elements(By.CSS_SELECTOR, 'td')
+                cells = await row.querySelectorAll('td')
                 for cell in cells:
-                    text = cell.text.strip()
+                    text = (await page.evaluate('(element) => element.textContent', cell)).strip()
                     time_data = extract_hours_and_minutes(text)
                     if time_data:
                         hours, minutes = time_data
@@ -84,11 +68,12 @@ async def run_selenium(email, password):
         else:
             return 'No table found with id="product-table"'
 
+    except TimeoutError as e:
+            return False
     except Exception as e:
-        return False
+            return False
     finally:
-        driver.quit()
-
+        await browser.close()
 
 def extract_hours_and_minutes(text):
     regex = re.compile(r'(\d+)\s*hrs,\s*(\d+)\s*min')
